e554ee8c945500d1490db59ac53c3aad96b6b67e
bit03class <bit01class@gmail.com>
2019-07-23 13:10:27 +0900

    bit framework - handleMapping step1

--- bitFramework01/src/com/bit/framework/DispatcherServlets.java
+++ bitFramework01/src/com/bit/framework/DispatcherServlets.java
@@ -2,18 +2,25 @@ package com.bit.framework;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.bit.controller.AddController;
-import com.bit.controller.IndexController;
-import com.bit.controller.ListController;
-import com.bit.controller.MainController;
-
 public class DispatcherServlets extends HttpServlet{
+	Map<String,String> map=new HashMap<String,String>();
+	
+	@Override
+	public void init() throws ServletException {
+		map.put("/index.bit", "com.bit.controller.IndexController");
+		map.put("/main.bit", "com.bit.controller.MainController");
+		map.put("/list.bit", "com.bit.controller.ListController");
+		map.put("/add.bit", "com.bit.controller.AddController");
+		map.put("/insert.bit", "com.bit.controller.InsertController");
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -34,18 +41,22 @@ public class DispatcherServlets extends HttpServlet{
 		String root=req.getContextPath();
 		String path=req.getRequestURI().substring(root.length());
 		
-		Controller controller =null;
+		// handleMapping
 		
-		if(path.equals("/index.bit")){
-			controller = new IndexController();
-		}else if(path.equals("/main.bit")){
-			controller = new MainController();
-		}else if(path.equals("/list.bit")){
-			controller = new ListController();
-		}else if(path.equals("/add.bit")){
-			controller = new AddController();
-		}else if(path.equals("/insert.bit")){
-			controller = new com.bit.controller.InsertController();
+		Controller controller =null;
+		String clInfo=null;
+		
+		clInfo=map.get(path);
+		
+		try {
+			Class clazz = Class.forName(clInfo);
+			controller=(Controller) clazz.newInstance();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
 		}
 		
 		String viewName=null;
