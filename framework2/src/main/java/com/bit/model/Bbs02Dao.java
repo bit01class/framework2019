81976a173c0e5f55e70993de1f689402ff307d54
bit03class <bit01class@gmail.com>
2019-07-24 10:25:43 +0900

    junit test ready

---
+++ framework2/src/main/java/com/bit/model/Bbs02Dao.java
@@ -0,0 +1,17 @@
+package com.bit.model;
+
+import javax.sql.DataSource;
+
+public class Bbs02Dao {
+	DataSource dataSource;
+
+	public Bbs02Dao() {
+		
+	}
+	public Bbs02Dao(DataSource dataSource) {
+		this.dataSource=dataSource;
+	}
+	
+	
+	
+}
